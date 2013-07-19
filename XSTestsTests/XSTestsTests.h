ae8e6e43ed92dd1b5dbd38dd40c238e31387f4c6
Xin Sun <xinsun@paypal.com>
2013-07-18 23:30:45 -0700

    Add test code

---
+++ XSTestsTests/XSTestsTests.h
@@ -0,0 +1,13 @@
+//
+//  XSTestsTests.h
+//  XSTestsTests
+//
+//  Created by Sun, Xin on 7/18/13.
+//  Copyright (c) 2013 Sun, Xin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XSTestsTests : SenTestCase
+
+@end
---
+++ XSTestsTests/XSTestsTests.m
@@ -0,0 +1,32 @@
+//
+//  XSTestsTests.m
+//  XSTestsTests
+//
+//  Created by Sun, Xin on 7/18/13.
+//  Copyright (c) 2013 Sun, Xin. All rights reserved.
+//
+
+#import "XSTestsTests.h"
+
+@implementation XSTestsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STAssertNil(nil, @"This should be a nil project");
+}
+
+@end
